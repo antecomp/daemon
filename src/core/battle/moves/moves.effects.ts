af9a0071a31f18e5f3a985ff1f829c9f3caf12ad
omni.vi <=>
2025-03-01 17:01:28 +0100

    bruh

--- src/core/battle/moves/moves.effects.ts
+++ src/core/battle/moves/moves.effects.ts
@@ -52,7 +52,7 @@ export const ApplySelfPrepared: MoveSideEffect = ({self, appendActionMessage}) =
 export const EvadePostEffect: PostMoveSideEffect = ({damageTaken, index, sequenceBuffer, self, appendActionMessage}) => {
     if(sequenceBuffer[index].evadeSuccessful) {
         // Confirm we actually dodged some incoming damage...
-        if(damageTaken === 0) {
+        if(damageTaken === 0) { // TODO PRIO: This is stupid and won't work. No shit we'll not take damage if we dodged nothing hfjkdkhsdfjksdf
             self.addStatus(new ManiaStatus(1));
             appendActionMessage(`${self.name} dodges swiftly. ${self.name} feels invigorated!`);
         }
