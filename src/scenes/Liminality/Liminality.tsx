bb85104a5bb97c42dd8c8027187048cf03fcb94a
antecomp <60552584+antecomp@users.noreply.github.com>
2025-03-30 21:45:47 +0200

    Extend bug demo

--- src/scenes/Liminality/Liminality.tsx
+++ src/scenes/Liminality/Liminality.tsx
@@ -8,7 +8,7 @@ import { onMount } from "solid-js";
 import applyShadows from '@/core/lume/applyShadows';
 import Interactable from '@/components/lume/Interactable';
 import Multicam, { cameraController } from '@/components/lume/multicam/Multicam';
-import { playerCam, snapTo } from '@/components/lume/multicam/multicam-behaviors';
+import { lerpTo, playerCam, snapTo } from '@/components/lume/multicam/multicam-behaviors';
 
 export default function Liminality() {
     let sceneRef: Scene | undefined;
@@ -27,6 +27,7 @@ export default function Liminality() {
 
     function handleDiamondClick() {
         cameraController.setTemporaryBehavior(snapTo("0 -512 141", 0, -22));
+        //cameraController.setTemporaryBehavior(lerpTo([0, -512, 141], 0, -22)); // This one doesn't require the function cleanup in playercam
     }
 
     return (
