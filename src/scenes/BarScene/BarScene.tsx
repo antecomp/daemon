ec515387af41df64e729a390e2c7cd1693060de1
omni.vi <60552584+antecomp@users.noreply.github.com>
2025-05-30 00:11:14 +0200

    tweaks to bar scene idea

--- src/scenes/BarScene/BarScene.tsx
+++ src/scenes/BarScene/BarScene.tsx
@@ -7,9 +7,11 @@ import barscene_glb from "./models/barscene.glb?url"
 import barobj from "./models/bar2.obj"
 import barmtl from "./models/bar2.mtl"
 import applyShadows from "@/core/lume/applyShadows";
+import starfield from "../shared_textures/starfield.png"
+import PlayerCam from "@/components/lume/playerCam/PlayerCam";
 
 export default function BarScene() {
-    let sceneRef: Scene | undefined;
+    let sceneRef!: Scene;
     let aaa!: ObjModel;
     
     onMount(() => {
@@ -29,12 +31,21 @@ export default function BarScene() {
             // fog-color="#000000"
             // fog-near="0" fog-far="1050"
         >
-            <WadsCam
+            {/* <WadsCam
                 defaultPosition="-719 -327 151"
+            /> */}
+
+            <PlayerCam
+                sceneRef={sceneRef}
+                basePos={[-719, -327, 151]}
+                baseOri={{yaw: 50, pitch: 0}}
+                maxPitch={20}
+                maxYaw={50}
             />
 
-            <lume-ambient-light intensity={2}/>
-            <lume-point-light position="-500 -150 20" intensity={1000} cast-shadow="true"/>
+            <lume-ambient-light intensity={1}/>
+            <lume-point-light position="-500 -180" intensity={1250} cast-shadow="true"/>
+            {/* <lume-point-light position="0 -100 0" intensity={200} cast-shadow="true"/> */}
             {/* <lume-directional-light position="201 -447 229" intensity={5} cast-shadow="true" /> */}
 
             {/* <lume-collada-model
@@ -65,6 +76,18 @@ export default function BarScene() {
                 scale="50 50 50"
                 receive-shadow="true"
                 cast-shadow="true"
+            />
+
+            <lume-sphere
+                id="stars"
+                texture={starfield}
+                receive-shadow="false"
+                //@ts-ignore
+                has="basic-material"
+                sidedness="back"
+                size="5000 5000 5000"
+                mount-point="0.5 0.5 0.5"
+                color="white"
             />
 
 
