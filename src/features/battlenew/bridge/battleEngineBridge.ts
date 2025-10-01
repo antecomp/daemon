3b45fb3924156ac29f5c9b721fb84c5feb1dca0b
omni.vi <60552584+antecomp@users.noreply.github.com>
2025-09-30 23:08:37 -0600

    kfdvhjnbg

--- src/features/battlenew/bridge/battleEngineBridge.ts
+++ src/features/battlenew/bridge/battleEngineBridge.ts
@@ -2,11 +2,24 @@ import { OpponentAI, OpponentStats } from "@/core/battlenew/ai/opponentAI.types"
 import { createBattleEngine } from "@/core/battlenew/engine/battleEngine";
 import { BattleReactions } from "@/core/battlenew/events/battleEvent.types";
 import { DamageMultipliers } from "@/core/battlenew/model/battle";
+import { PlannedMove } from "@/core/battlenew/model/plannedmove";
 import { createSignal } from "solid-js";
 
 export enum BattleUIState {
     WAITING, READY, EXECUTING, 
     END // Temporary state when animating the UI closing on battle end.    
+}
+
+const HINT_AMOUNT = 3;
+
+const generateHint = (seq: PlannedMove[]): (string | undefined)[] => { /* Later this should return some nicer interface. */
+    const indices = new Set<number>
+
+    while (indices.size < HINT_AMOUNT) {
+        indices.add(Math.floor(Math.random() * seq.length));
+    }
+
+    return seq.map((item, index) => indices.has(index) ? undefined : item.name);
 }
 
 export function createUIBridedBattleEngine(opponentAI: OpponentAI, opponentStats: OpponentStats) {
@@ -17,13 +30,27 @@ export function createUIBridedBattleEngine(opponentAI: OpponentAI, opponentStats
     const [battleUIState, setBattleUIState] = createSignal<BattleUIState>(BattleUIState.WAITING);
     const [playerHealthPercentage, setPlayerHealthPercentage] = createSignal(100);
     const [opponentHealthPercentage, setOpponentHealthPercentage] = createSignal(100);
-    const [opponentPlanPreview, setOpponentPlanPreview] = createSignal<string[]>([]); // will just do names until we have proper mapping code.
+    const [opponentPlanPreview, setOpponentPlanPreview] = createSignal<(string | undefined)[]>([]); // will just do names until we have proper mapping code.
 
     // holding off on the current statuses thing until I have more mapping info.
 
     // Make reactions here! Will likely split up into smaller helpers later.
     const reactions: BattleReactions = {
-        
+        RoundStart({plans}) {
+            // will do a proper mapper later;
+            // will add UI animations here.
+            setOpponentPlanPreview(generateHint(plans.opponent))
+        },
+
+        MultipliersComputed({damageMultipliers}) {
+            setPlayerMults(damageMultipliers.player);
+            setOpponentMults(damageMultipliers.opponent);
+        },
+
+        DamagesApplied({combatants}) {
+            setPlayerHealthPercentage(combatants.player.healthPercent);
+            setOpponentHealthPercentage(combatants.opponent.healthPercent)
+        }
     };
 
 
