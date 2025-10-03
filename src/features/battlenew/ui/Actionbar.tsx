d12e167581a9696528cecca686f9a101c8bc6fed
omni.vi <60552584+antecomp@users.noreply.github.com>
2025-10-02 23:13:00 -0600

    Poke at UI drafting

--- src/features/battlenew/ui/Actionbar.tsx
+++ src/features/battlenew/ui/Actionbar.tsx
@@ -8,9 +8,10 @@ import ur_bar from '../assets/mult_ur.png'
 import us_bar from '../assets/mult_us.png'
 import dr_bar from '../assets/mult_dr.png'
 import ds_bar from '../assets/mult_ds.png'
-import { Accessor } from 'solid-js'
-import { DamageMultipliers } from '@/core/battlenew/model/battle'
-import { useBattleUIState } from '../Battle'
+import { Accessor, For } from 'solid-js'
+import { BattleOutcome, DamageMultipliers } from '@/core/battlenew/model/battle'
+import { BattleUIState, useBattleUIState } from '../Battle'
+import { PlannedSequence } from '@/core/battlenew/model/plannedmove'
 
 interface SelectedMoveProps {
     icon?: string // img url
@@ -53,7 +54,65 @@ function mapMultiplier(multiplier: number): number {
 export default function Actionbar(props: ActionbarProps) {
     const {battleUIState, setBattleUIState} = useBattleUIState();
 
+    // TODO: DECIDE ON PROPER MAPPING SYSTEM FOR UI MOVES TO MOVES
+    // DO WE DO A MAPPING OR WRAPPER?
+
+    const handleEject = () => {
+        if(battleUIState() == BattleUIState.READY || battleUIState() == BattleUIState.WAITING) {
+            props.forceBattleResolve(BattleOutcome.PlayerEject);
+        }
+    }
+
     return (
-        <></>
+        <div id="battle-actionbar">
+            <div class="left">
+                <img src={eject_button} id="eject-button"
+                    onClick={handleEject}
+                />
+            </div>
+            <Runebuilder />
+            <div id="rb-buttons">
+                <img
+                    id='reset-button'
+                    src={reset_button}
+                // onClick={resetRunes} 
+                // classList={{usable: sequenceBuffer().length > 0}}
+                />
+                <img
+                    id='exec-button'
+                    src={exec_button}
+                    classList={{ usable: (battleUIState() == BattleUIState.READY) }}
+                // onClick={handleExecClick}
+                />
+            </div>
+            <div class="right">
+                <div class="moves">
+                    {/* <For each={sequenceBuffer()}>
+                        {(x) => <SelectedMove {...x} />}
+                    </For> */}
+                </div>
+                <img src={fch_bar} id="fch-bar" style={`--level: ${props.playerHealthPercentage()}%`} />
+                <div id="multbars">
+                    <div class="userbars">
+                        <img src={ur_bar} class="ur-bar" style={`--level: ${mapMultiplier(props.playerMults().incoming)}%`} />
+                        <img src={us_bar} class="us-bar" style={`--level: ${mapMultiplier(props.playerMults().outgoing)}%`} />
+                    </div>
+                    <div class="dbars">
+                        <img src={ds_bar} class="ds-bar" style={`--level: ${mapMultiplier(props.opponentMults().outgoing)}%`} />
+                        <img src={dr_bar} class="dr-bar" style={`--level: ${mapMultiplier(props.opponentMults().incoming)}%`} />
+                    </div>
+                    <div class="player-statuses">
+                        {/* <For each={props.currentStatuses().player}>
+                            {stat => <img class="status-icon" src={stat} />}
+                        </For> */}
+                    </div>
+                    <div class="opp-statuses">
+                        {/* <For each={props.currentStatuses().opp}>
+                            {stat => <img class="status-icon" src={stat} />}
+                        </For> */}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
