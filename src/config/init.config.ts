04fd5e53f25414f4bb19d407997d19ee1233776e
omni.vi <60552584+antecomp@users.noreply.github.com>
2025-12-18 21:49:01 -0700

    test bake and render of new bar

--- src/config/init.config.ts
+++ src/config/init.config.ts
@@ -1,3 +1,3 @@
-export const INITIAL_SCENE = "Islands";
+export const INITIAL_SCENE = "TheGem";
 export const LOGIN_MESSAGE = "INSTANCE INIT | [ DAEMON-GARDEN v.0.600 ]"
 export const MAIN_CHARACTER_NAME = "Arda";
---
+++ src/scenes/TheGem/TheGem.tsx
@@ -0,0 +1,32 @@
+import { Scene } from "lume";
+import bar_model from './assets/bar.glb?url'
+import PlayerCam from "@/3d/camera/PlayerCam";
+import Freecam from "@/3d/camera/Freecam";
+import { useDGShader } from "@/3d/pipeline/dgRender";
+
+export default function TheGem() {
+    let sceneRef!: Scene;
+
+    useDGShader(() => sceneRef);
+
+    return (
+        <lume-scene
+            webgl
+            ref={sceneRef}
+            id='SCENE'
+            shadow-mode="pcf"
+            perspective="800"
+            shadowmap-type="pcfsoft"
+        >
+            <lume-ambient-light id="ambientLight" intensity={12}/>
+            <lume-gltf-model
+                src={bar_model}
+                id="bar"
+                scale="100 100 100"
+                align-point="0.5 0.5"
+            />
+
+            <Freecam sceneRef={sceneRef}/>
+        </lume-scene>
+    )
+}
