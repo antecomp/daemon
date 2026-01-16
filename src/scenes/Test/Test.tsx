a4f14d41b828f7136010e9e63cd60819ee64d33a
omni.vi <60552584+antecomp@users.noreply.github.com>
2026-01-15 21:50:26 -0700

    add sprite asset to test scene

--- src/scenes/Test/Test.tsx
+++ src/scenes/Test/Test.tsx
@@ -5,7 +5,11 @@ import createTileNavigator from '@/3d/tilenav/createTileNavigator';
 import { NavMap } from '@/3d/tilenav/tilenav.types';
 import { Scene } from 'lume';
 import NavTilePreviewer from '@/3d/tilenav/NavTilePreviewer';
+import girl_sprite from './assets/girl2.png';
 import { useDGShader } from '@/3d/pipeline/dgRender';
+import Freecam from '@/3d/camera/Freecam';
+import Billboard from '@/3d/components/Billboard';
+import { addLogMessage } from '@/app/shell/hud/EventLog';
 
 export default function Test() {
 
@@ -15,28 +19,42 @@ export default function Test() {
 
     return (
         <lume-scene
-                    ref={sceneRef}
-                    webgl
-                    shadow-mode="basic"
-                    id='SCENE'
-                    physically-correct-lights
-                    perspective="800"
-                >
+            ref={sceneRef}
+            webgl
+            shadow-mode="basic"
+            id='SCENE'
+            physically-correct-lights
+            perspective="800"
+        >
 
-                    <PlayerCam
+            <PlayerCam
                         sceneRef={sceneRef}
                         {...sig()}
                     />
+            {/* <Freecam sceneRef={sceneRef} initialPos={[-45, -81, -98]} initialOri={{yaw:-253, pitch:0}}/> */}
 
-                    <lume-ambient-light intensity={4} />
+            <lume-ambient-light intensity={4} />
 
-                    <lume-gltf-model
-                        align-point="0.5 0.5"
-                        scale="100 100 100"
-                        src={world}
-                    />
+            <lume-gltf-model
+                align-point="0.5 0.5"
+                scale="100 100 100"
+                src={world}
+            />
 
-                    {/* <NavTilePreviewer NM={NM as NavMap}/> */}
+            <Billboard
+                texture={girl_sprite}
+                position='-450 -20 -2'
+                scale={90}
+                interactions={
+                    [
+                        () => addLogMessage('hello.'),
+                        () => addLogMessage('she has nothing to say'),
+                        () => addLogMessage('you don\'t know why, but her presence fills you with rage', 'red')
+                    ]
+                }
+            />
+
+            {/* <NavTilePreviewer NM={NM as NavMap}/> */}
         </lume-scene>
     )
 }
