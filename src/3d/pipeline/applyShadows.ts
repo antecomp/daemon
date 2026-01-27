a691a16aef53dbe4fd9f063e1541cfa1999c90b0
omni.vi <60552584+antecomp@users.noreply.github.com>
2026-01-26 23:55:46 -0700

    applyShadows - remove stupid ternary and switch to default params

--- src/3d/pipeline/applyShadows.ts
+++ src/3d/pipeline/applyShadows.ts
@@ -2,7 +2,7 @@ import { Element3D, ObjModel } from "lume";
 
 
 /** Recursively enables shadows (including self-shadowing) on all subcomponents of model. Can be used to add shadows to a map component. */
-export default function applyShadows(ref: Element3D | ObjModel, cast?: boolean, receive?: boolean) {
+export default function applyShadows(ref: Element3D | ObjModel, cast = true, receive = true) {
 
     // Cursed but functional alternative (keepin just in case).
     // if(ref.three.children.length == 0) {
@@ -25,8 +25,8 @@ export default function applyShadows(ref: Element3D | ObjModel, cast?: boolean, 
         ref.three.traverse(n => {
             //@ts-ignore // (property does exist but it's not typed in)
             if(!n.isMesh) return;
-            n.castShadow = cast === undefined ? true : cast;
-            n.receiveShadow = receive === undefined ? true : receive;
+            n.castShadow = cast;
+            n.receiveShadow = receive;
         });
     })
 
