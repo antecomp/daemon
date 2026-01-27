1302fb4508713f151abd05de80c21a90824deb54
omni.vi <60552584+antecomp@users.noreply.github.com>
2026-01-26 23:32:22 -0700

    basic login mockup

--- src/config/init.config.ts
+++ src/config/init.config.ts
@@ -1,5 +1,7 @@
+export const VERSION = "0.0785"
+
 export const INITIAL_SCENE = "Bridge";
-export const LOGIN_MESSAGE = "INSTANCE INIT | [ DAEMON-GARDEN v.0.702 ]"
+export const LOGIN_MESSAGE = `INSTANCE INIT | [ DAEMON-GARDEN v.${VERSION} ]`
 export const MAIN_CHARACTER_NAME = "Arda";
 
 export const SKIP_NEW_GAME_LOGIN = false;
--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,13 +1,17 @@
 import { Setter } from "solid-js";
 import ASCM_LOGO from '@/assets/ui/misc/ASCM.png';
 import { playTextOverlay } from "../text-overlay/TextOverlay";
-
+import './login.css';
+import { VERSION } from "@/config/init.config";
 
 /* Fake login screen used for game init. */
 export default function Login(props: { setGameStart: Setter<boolean> }) {
     return <div id="login-screen">
         <img src={ASCM_LOGO} />
-        <p>Login Screen Here?</p>
-        <button onClick={() => {props.setGameStart(true); playTextOverlay([["Cool Login Message!"]], true)}}>Login (New Game)</button>
+        <h1>Welcome back, <span style={{color: 'yellow'}}>Arda</span></h1>
+        <p>Please insert the <span style={{color: 'red'}}>VI-LINK</span> cable into your cerebral interface and press login.</p>
+        <br />
+        <button onClick={() => {props.setGameStart(true); playTextOverlay([["Cool Login Message!"]], true)}}>LOGIN</button>
+        <footer>daemon.garden version {VERSION}, created by <a href="https://omni.vi">omni.vi</a></footer>
     </div>
 }
