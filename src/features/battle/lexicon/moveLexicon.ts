2b5a0f50129c7ed9a39f55a59255cc0753a232ec
omni.vi <60552584+antecomp@users.noreply.github.com>
2026-02-19 15:45:16 -0700

    dry out moveLexicon player defs

--- src/features/battle/lexicon/moveLexicon.ts
+++ src/features/battle/lexicon/moveLexicon.ts
@@ -56,7 +56,7 @@ export const COMMON_MOVE_LEXICON: MoveLexicon = {
         label: "evade",
         icon: BATTLE_RUNE_IMGS.trickster,
         largeIcon: BATTLE_RUNE_IMGS.trickster_ex,
-        description: 'Chance to negate all incoming damage. When successful, a followup attack will do extra damage.'
+        description: 'Chance to evade incoming damage. When successful, a followup attack will do extra damage.'
     },
 
     heal: {
@@ -118,75 +118,56 @@ export const COMMON_MOVE_LEXICON: MoveLexicon = {
         label: 'observe',
         icon: BATTLE_RUNE_IMGS.lantern,
         largeIcon: BATTLE_RUNE_IMGS.lantern_ex,
-        description: 'Makes opponent vulnerable.'
+        description: 'Makes target vulnerable.'
     }
 }
 
-export const PLAYER_MOVE_LEXICON: MoveLexicon = {
+// Utility stuff to keep the player lexicon declarations a bit more DRY.
+type MoveLexiconOverlays = Partial<Record<MoveLexeme, MoveDisplayOverride>>;
+const overlayMoveLexicon = (base: MoveLexicon, overlays: MoveLexiconOverlays): MoveLexicon => {
+    return Object.fromEntries(
+        Object.entries(base).map(([moveName, moveEntry]) => {
+            const move = moveName as MoveLexeme;
+            return [move, { ...moveEntry, ...overlays[move] }];
+        })
+    ) as MoveLexicon;
+};
 
-    ...COMMON_MOVE_LEXICON,
-
+const PLAYER_MOVE_LEXICON_OVERLAYS: MoveLexiconOverlays = {
     repeat: {
         label: "apprentice",
-        icon: BATTLE_RUNE_IMGS.apprentice,
-        largeIcon: BATTLE_RUNE_IMGS.apprentice_ex,
         lore: `Like the flowers, knowledge comes from the rotting ones.`,
-        description: 'Repeat previous rune. Cannot be used first.'
     },
-
     evade: {
         label: "trickster",
-        icon: BATTLE_RUNE_IMGS.trickster,
-        largeIcon: BATTLE_RUNE_IMGS.trickster_ex,
         lore: `Our first understanding of self comes from a two-faced fox.`,
-        description: 'Chance to negate all incoming damage. When successful, a followup attack will do extra damage.'
     },
-
     heal: {
         label: "priestess",
-        icon: BATTLE_RUNE_IMGS.priestess,
-        largeIcon: BATTLE_RUNE_IMGS.priestess_ex,
         lore: `We only stay for the pretty music.`,
-        description: 'When not attacked, restore health.'
     },
-
     prepare: {
         label: "hourglass",
-        icon: BATTLE_RUNE_IMGS.hourglass,
-        largeIcon: BATTLE_RUNE_IMGS.hourglass_ex,
         lore: `The sand is nauseous from your constant turmoil.`,
-        description: 'If not attacked, increase effectiveness of subsequent rune.'
     },
-
     defend: {
         label: "praetorian",
-        icon: BATTLE_RUNE_IMGS.prae,
-        largeIcon: BATTLE_RUNE_IMGS.prae_ex,
         lore: `The bravest coward you'll ever meet.`,
-        description: 'Reduce incoming damage.'
     },
-
     attack: {
         label: "candlelight",
         icon: BATTLE_RUNE_IMGS.candle,
         largeIcon: BATTLE_RUNE_IMGS.candle_ex,
         lore: `If moonlight heals, what does candlelight do?`,
-        description: 'Deal damage.'
     },
-
     overwhelm: {
         label: "overwhelm",
-        icon: BATTLE_RUNE_IMGS.chains,
-        largeIcon: BATTLE_RUNE_IMGS.chain_ex,
         lore: `We are still ultimately animals.`,
-        description: 'Only does damage to Defensive/Evasive moves (negating their reduction). Makes user vulnerable on use.'
     },
-
     mirror: {
         label: "mirror",
-        icon: BATTLE_RUNE_IMGS.mage,
-        largeIcon: BATTLE_RUNE_IMGS.mage_ex,
         lore: `Distorted truths cut like knives.`,
-        description: 'Perform same action as opponent.'
     },
-}
+};
+
+export const PLAYER_MOVE_LEXICON: MoveLexicon = overlayMoveLexicon(COMMON_MOVE_LEXICON, PLAYER_MOVE_LEXICON_OVERLAYS);
