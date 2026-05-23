dbb2ce4f9bec2906d0dc1a8b72fa7e33874b473a
antecomp <60552584+antecomp@users.noreply.github.com>
2026-05-23 13:17:14 -0600

    slop

--- src/scenes/TheGem/data/decrypt_textscene.ts
+++ src/scenes/TheGem/data/decrypt_textscene.ts
@@ -6,7 +6,7 @@ const decrypt_textscene: TextOverlaySequence = [
     ["-and my vision blurs as my ", ['VI-LINK', 'red'], ' resets.'],
     ["Once I come to, I read the contents of the diskette again."],
     ["Whatever code ran deleted itself, the cache only contains some coordinates and nothing else."],
-    {segments: [['"This must be the meeting spot"', 'teal'], "\n I say to myself."], sideEffect() {setCurrentScene('Bridge')}}
+    {segments: [['"This must be the spot"', 'yellow'], "\n I say to myself."], sideEffect() {setCurrentScene('Bridge')}}
     // Consider something along the lines of "I enter the coordinates into my navigator... Indicate movement and the passage of time better."
 ]
 
